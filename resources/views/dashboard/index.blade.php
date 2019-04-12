1cb5f9c2ba5f751e0a3db9598db971c237ea14f9
vagrant <vagrant@homestead>
2019-04-12 16:49:26 +0000

    Created DashboardController + change login/registration redirect

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2>Git repos list</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <table class="table">
+                    <thead>
+
+                    </thead>
+                </table>
+            </div>
+        </div>
+    </section>
+@endsection
