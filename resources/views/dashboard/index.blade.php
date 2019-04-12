b148be448f1ccb794d877a3f245bead58b65a888
vagrant <vagrant@homestead>
2019-04-12 17:19:01 +0000

    added resourses/sass/content/dashboard/_reposlist

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,7 +4,7 @@
     <section class="container">
         <div class="row">
             <div class="col-12">
-                <h2>Git repos list</h2>
+                <h2 class="repos_title">Git repos list</h2>
             </div>
         </div>
         <div class="row">
